34fc6bb216a7f8a032a50560476c2e32105747b0
renevoorburg <voorburg@xs4all.nl>
2025-06-15 10:38:36 +0200

    feat: add English translations and refine Dutch marker definitions

---
+++ lang/lang_en.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+    // defaults from base.tpl:
+    // 'archaeological atlas' => 'archeologische atlas',
+    // 'search' => 'Zoeken',
+    // 'Add' => 'Toevoegen',
+    // 'Login / Register' => 'Login / Register',
+    // 'More about Vici.org' => 'Meer over Vici.org',
+    // 'Recently added' => 'Recent toegevoegd',
+    // 'Recently changed' => 'Recent aangepast',
+    // 'About Vici.org' => 'Over Vici.org',
+
+    // // defaults from home.tpl:
+    // 'Highlights' => 'Uitgelicht',
+
+    // // 
+    // 'more' => 'meer',
+    // 'show on map' => 'toon op kaart',
+
+    // keyed definitions:
+    'markerdef.1' => 'An aqueduct, above or below ground, or a part of it',
+    'markerdef.2' => 'A bath or a bath complex',
+    'markerdef.3' => 'A settlement with administrative, economic or religious functions of more than local significance',
+    'markerdef.4' => 'A fort or a larger military defensive structure',
+    'markerdef.5' => 'A burial field, tombstone or a grave',
+    'markerdef.6' => 'A workshop or an industry, for example a harbor or mine',
+    'markerdef.7' => 'A hotel or rest stop',
+    'markerdef.8' => 'A contemporary museum with archaeological artifacts',
+    'markerdef.9' => 'A settlement of primary local significance',
+    'markerdef.10' => 'A shipwreck or the location of a shipwreck',
+    'markerdef.11' => 'A temple, church or other religious place',
+    'markerdef.12' => 'A place or building for performances for a public audience',
+    'markerdef.13' => 'A rural residence associated with agricultural activity and used as a dwelling by people of higher status',
+    'markerdef.14' => 'A rural settlement',
+    'markerdef.15' => 'A watchtower or a small military structure',
+    'markerdef.16' => 'A monument with public or religious significance',
+    'markerdef.17' => 'A place of an archaeological find',
+    'markerdef.18' => 'A place of archaeological observation',
+    'markerdef.19' => 'A modern monument or replica, related to historical events',
+    'markerdef.20' => 'A milestone or a road marker, or the original place of it',
+    'markerdef.21' => 'A building, or another fabricated structure',
+    'markerdef.22' => 'A bridge',
+    'markerdef.23' => 'A paved or hardened road',
+    'markerdef.24' => 'A place of a historical event',
+    'markerdef.25' => 'A military structure of temporary nature',
+
+
+];
