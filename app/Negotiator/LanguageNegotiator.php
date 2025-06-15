613587a23af2a83390cbc4d5712976c67095d505
renevoorburg <voorburg@xs4all.nl>
2025-06-15 12:38:58 +0200

    refactor: dry language get param name definition

--- app/Negotiator/LanguageNegotiator.php
+++ app/Negotiator/LanguageNegotiator.php
@@ -2,11 +2,13 @@
 
 namespace Vici\Negotiator;
 
+const LANG_PARAM_NAME = 'lang';
+
 class LanguageNegotiator extends Negotiator {
     protected $headerName = 'HTTP_ACCEPT_LANGUAGE';
-    protected $urlParamName = 'lang';
+    protected $urlParamName = LANG_PARAM_NAME;
 
     public static function hasForcedLanguage() {
-        return !empty($_GET['lang']);
+        return !empty($_GET[LANG_PARAM_NAME]);
     }
 }
