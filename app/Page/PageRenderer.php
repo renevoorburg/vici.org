79b71c1b095b1f18156e92d4ff0e9e2323bc0c7e
renevoorburg <voorburg@xs4all.nl>
2026-06-03 19:31:57 +0200

    feat: add 'lang' template variable as alias for session language

--- app/Page/PageRenderer.php
+++ app/Page/PageRenderer.php
@@ -27,6 +27,7 @@ class PageRenderer extends Smarty
         $this->assign('availableLanguages', $this->session->getAvailableLanguages());
         $this->assign('viciToken', $_ENV['VICITOKEN']);
         $this->assign('sessionLanguage', $this->session->getLanguage());
+        $this->assign('lang', $this->session->getLanguage());
         if (isset($_ENV['VICIBASE'])) {
             $this->assign('vicibase', $_ENV['VICIBASE']);
         }
